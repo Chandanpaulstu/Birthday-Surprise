4364b794df007f7b5f7f2a9a320d4e0cbbef7956
Chandanpaulstu <chandanpaulstu@gmail.com>
2023-09-08 20:09:41 +0600

    .

--- effect.js
+++ effect.js
@@ -188,7 +188,7 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(100).promise().done(function(){
+			$("p:nth-child("+i+")").fadeOut('slow').delay(1000).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(2000);
 			if(i==50){
